c665d0503987052c140ee880a6e9a0b030cfed1c
Lucas Garron <code@garron.net>
2021-11-07 12:41:27 -0800

    Standardize country names.

--- admin/process-responses/src/data.ts
+++ admin/process-responses/src/data.ts
@@ -1,16 +1,16 @@
 export const year = 2021;
 
 export const data = `Timestamp,Name,Country Where You Participated,Event,Result,Comment (optional),Video URL (optional),Solve Reconstruction (optional)
-10/30/2021 19:02:13,Devin Corr-Robinett,portugal,3x3x3 Speedsolve,"-59min, 49.10sec",,,
+10/30/2021 19:02:13,Devin Corr-Robinett,Portugal,3x3x3 Speedsolve,"-59min, 49.10sec",,,
 10/31/2021 13:09:48,Vlad Hordiienko,Ukraine ,3x3x3 Speedsolve,"-59min, 49.38sec",good first pair,,
-11/6/2021 23:04:13,Chris Hardwick,United States,3x3x3 Speedsolve,"-59 min, 46.39sec","This is a lot harder to do each year as I get older, but I still enjoy it!",,
-11/6/2021 23:07:00,Tyler Farr,United States,3x3x3 Speedsolve,"-59min, 47.313sec",,,
-11/7/2021 0:01:18,James Quinn,United States,3x3x3 Speedsolve,"-59min, 51.92sec",,,
+11/6/2021 23:04:13,Chris Hardwick,USA,3x3x3 Speedsolve,"-59 min, 46.39sec","This is a lot harder to do each year as I get older, but I still enjoy it!",,
+11/6/2021 23:07:00,Tyler Farr,USA,3x3x3 Speedsolve,"-59min, 47.313sec",,,
+11/7/2021 0:01:18,James Quinn,USA,3x3x3 Speedsolve,"-59min, 51.92sec",,,
 11/7/2021 0:04:10,Jeremy Fleischman,USA,3x3x3 Speedsolve,"-59min, 46.65sec",Hello from the Wisconsin beach. Soo sleepy.,,
-11/7/2021 0:06:08,Drew Forbush,United States of America,3x3x3 Mirror Cube,"-57 min, 63.55 sec",My cube is missing a piece lol,,
+11/7/2021 0:06:08,Drew Forbush,USA,3x3x3 Mirror Cube,"-57 min, 63.55 sec",My cube is missing a piece lol,,
 11/7/2021 1:02:15,David Reid,USA,3x3x3 Speedsolve,"-59min, 43.41sec",,,
 11/7/2021 1:02:25,Michael Kastner,USA,3x3x3 Speedsolve,"-59min, 21.14sec",,,
-11/7/2021 1:02:55,Stone Amsbaugh,United States,3x3x3 Speedsolve,"-59min, 42.25sec",,,
+11/7/2021 1:02:55,Stone Amsbaugh,USA,3x3x3 Speedsolve,"-59min, 42.25sec",,,
 11/7/2021 1:26:09,Lyle Lowry,USA,2x2x2,"-59min, 45.45sec",,,
 11/7/2021 1:27:50,Lyle Lowry,USA,3x3x3 Speedsolve,"-59min, 13.54sec",,,
 11/7/2021 1:29:52,Lyle Lowry,USA,Skewb,"-59min, 2.3sec",,,
@@ -19,7 +19,7 @@ export const data = `Timestamp,Name,Country Where You Participated,Event,Result,
 11/7/2021 1:46:11,Lyle Lowry,USA,3x3x3 With Feet,"-53min, 11.69sec","Wow, I improved by roughly an hour. Now I'll definitely be ready for the next competition!",,
 11/7/2021 1:48:07,Lyle Lowry,USA,5x5x5,"-49min, 20.98sec",,,
 11/7/2021 1:50:39,Lyle Lowry,USA,7x7x7,"-35min, 49.38sec","My first ever sub 10, nice",,
-11/7/2021 2:07:00,Lucas Garron,United States,3x3x3 Speedsolve,"-59min, 49.25sec","I was initially a little disappointed, but I think it was actually not too bad considering I got cases with some very long algs. I managed very nearly 10tps for ELS + CLS + PLL!",https://youtu.be/8jfRVfI6ki4,"D F l U' r // Cross
+11/7/2021 2:07:00,Lucas Garron,USA,3x3x3 Speedsolve,"-59min, 49.25sec","I was initially a little disappointed, but I think it was actually not too bad considering I got cases with some very long algs. I managed very nearly 10tps for ELS + CLS + PLL!",https://youtu.be/8jfRVfI6ki4,"D F l U' r // Cross
 U' R U' R' d' R' U' R // Slot 1
 U' U2' R U R' y' R U R' // Slot 2
 (U' y) R U R' // Slot 3
