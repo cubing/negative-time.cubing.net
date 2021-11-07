51edb0fde3debf627eb75e6d2f9a0815af4c74d9
Lucas Garron <code@garron.net>
2021-11-07 03:40:31 -0800

    FREEZE.unindexed

--- admin/process-responses/src/index.ts
+++ admin/process-responses/src/index.ts
@@ -54,107 +54,110 @@ function format_centiseconds(result: number): string {
     .padStart(2, "0")}sec`;
 }
 
-const root = document.body;
-root.innerHTML = "";
-const tableRoot = root.appendChild(document.createElement("div"));
-for (const [event, rows] of Object.entries(by_event)) {
-  if (rows.length === 0) {
-    continue;
-  }
-  // TODO: handle ties
-  const sorted_rows = rows.sort(
-    (a, b) => centiseconds(a.Result) - centiseconds(b.Result)
-  );
-
-  const anyRowHasField = (fieldName: string): boolean => {
-    for (const row of rows) {
-      if (row[fieldName] !== "") {
-        console.log(fieldName, row);
-        return true;
-      }
+async function main() {
+  const root = document.body;
+  root.innerHTML = "";
+  const tableRoot = root.appendChild(document.createElement("div"));
+  for (const [event, rows] of Object.entries(by_event)) {
+    if (rows.length === 0) {
+      continue;
     }
-    return false;
-  };
-  const anyRowHasVideoURL = anyRowHasField(FieldName.VideoURL);
-  const anyRowHasReconstruction = anyRowHasField(FieldName.Reconstruction);
+    // TODO: handle ties
+    const sorted_rows = rows.sort(
+      (a, b) => centiseconds(a.Result) - centiseconds(b.Result)
+    );
 
-  const h2 = tableRoot.appendChild(document.createElement("h2"));
-  h2.id = `results-${eventData[event].code}`;
-  h2.append(event); // TODO: ID
-
-  const table = tableRoot.appendChild(document.createElement("table"));
-
-  const thead = table.appendChild(document.createElement("thead"));
-  const theadTr = thead.appendChild(document.createElement("tr"));
-  theadTr.appendChild(document.createElement("td")).append("Rank");
-  theadTr.appendChild(document.createElement("td")).append("Name");
-  theadTr.appendChild(document.createElement("td")).append("Country");
-  theadTr.appendChild(document.createElement("td")).append("Negative Time");
-  if (anyRowHasVideoURL) {
-    theadTr.appendChild(document.createElement("td")).append("🎥");
-  }
-  if (anyRowHasReconstruction) {
-    theadTr.appendChild(document.createElement("td")).append("▶️");
-  }
-  theadTr.appendChild(document.createElement("td")).append("Comment");
-
-  const tbody = table.appendChild(document.createElement("tbody"));
-  let rank = 0;
-
-  for (const row of sorted_rows) {
-    const tr = tbody.appendChild(document.createElement("tr"));
-    rank++;
-
-    const newTD = (): HTMLTableCellElement =>
-      tr.appendChild(document.createElement("td"));
-
-    newTD().append(rank.toString());
-    newTD().append(row.Name);
-    newTD().append(row[FieldName.Country]);
-    const formattedResult = format_centiseconds(centiseconds(row.Result));
-    newTD().append(formattedResult);
-    if (anyRowHasVideoURL) {
-      const videoTD = newTD();
-      const videoURL = row[FieldName.VideoURL];
-      if (videoURL !== "") {
-        const a = videoTD.appendChild(document.createElement("a"));
-        a.href = videoURL;
-        const span = a.appendChild(document.createElement("span"));
-        span.title = "Video";
-        span.textContent = "🎥";
+    const anyRowHasField = (fieldName: string): boolean => {
+      for (const row of rows) {
+        if (row[fieldName] !== "") {
+          console.log(fieldName, row);
+          return true;
+        }
       }
+      return false;
+    };
+    const anyRowHasVideoURL = anyRowHasField(FieldName.VideoURL);
+    const anyRowHasReconstruction = anyRowHasField(FieldName.Reconstruction);
+
+    const h2 = tableRoot.appendChild(document.createElement("h2"));
+    h2.id = `results-${eventData[event].code}`;
+    h2.append(event); // TODO: ID
+
+    const table = tableRoot.appendChild(document.createElement("table"));
+
+    const thead = table.appendChild(document.createElement("thead"));
+    const theadTr = thead.appendChild(document.createElement("tr"));
+    theadTr.appendChild(document.createElement("td")).append("Rank");
+    theadTr.appendChild(document.createElement("td")).append("Name");
+    theadTr.appendChild(document.createElement("td")).append("Country");
+    theadTr.appendChild(document.createElement("td")).append("Negative Time");
+    if (anyRowHasVideoURL) {
+      theadTr.appendChild(document.createElement("td")).append("🎥");
     }
     if (anyRowHasReconstruction) {
-      const reconstructionTD = newTD();
-      const reconstructionString = row[FieldName.Reconstruction];
-      if (reconstructionString !== "") {
-        const a = reconstructionTD.appendChild(document.createElement("a"));
-        console.log(eventData[event].scrambleString);
-        const player = new TwistyPlayer({
-          experimentalSetupAlg: eventData[event].scrambleString,
-          alg: reconstructionString,
-        });
-        a.href = await player.experimentalModel.viewerLinkProp.get();
-        // algCubingNetLink({
-        //   setup: algParse(eventData[event].scrambleString), // TODO
-        //   alg: algParse(reconstructionString),
-        //   title: `${row.Name}\nNegative Time Solve ${year}\n${formattedResult}`,
-        // });
-        const span = a.appendChild(document.createElement("span"));
-        span.title = "Reconstruction";
-        span.textContent = "▶️";
-      }
+      theadTr.appendChild(document.createElement("td")).append("▶️");
     }
-    newTD().append(row[FieldName.Comment]);
-  }
-}
+    theadTr.appendChild(document.createElement("td")).append("Comment");
 
-const button = document.createElement("button");
-root.prepend(button);
-button.textContent = "Copy";
-button.addEventListener("click", () => {
-  navigator.clipboard.writeText(
-    new XMLSerializer().serializeToString(tableRoot)
-  );
-  button.textContent = "Copied!";
-});
+    const tbody = table.appendChild(document.createElement("tbody"));
+    let rank = 0;
+
+    for (const row of sorted_rows) {
+      const tr = tbody.appendChild(document.createElement("tr"));
+      rank++;
+
+      const newTD = (): HTMLTableCellElement =>
+        tr.appendChild(document.createElement("td"));
+
+      newTD().append(rank.toString());
+      newTD().append(row.Name);
+      newTD().append(row[FieldName.Country]);
+      const formattedResult = format_centiseconds(centiseconds(row.Result));
+      newTD().append(formattedResult);
+      if (anyRowHasVideoURL) {
+        const videoTD = newTD();
+        const videoURL = row[FieldName.VideoURL];
+        if (videoURL !== "") {
+          const a = videoTD.appendChild(document.createElement("a"));
+          a.href = videoURL;
+          const span = a.appendChild(document.createElement("span"));
+          span.title = "Video";
+          span.textContent = "🎥";
+        }
+      }
+      if (anyRowHasReconstruction) {
+        const reconstructionTD = newTD();
+        const reconstructionString = row[FieldName.Reconstruction];
+        if (reconstructionString !== "") {
+          const a = reconstructionTD.appendChild(document.createElement("a"));
+          console.log(eventData[event].scrambleString);
+          const player = new TwistyPlayer({
+            experimentalSetupAlg: eventData[event].scrambleString,
+            alg: reconstructionString,
+          });
+          a.href = await player.experimentalModel.twizzleLink();
+          // algCubingNetLink({
+          //   setup: algParse(eventData[event].scrambleString), // TODO
+          //   alg: algParse(reconstructionString),
+          //   title: `${row.Name}\nNegative Time Solve ${year}\n${formattedResult}`,
+          // });
+          const span = a.appendChild(document.createElement("span"));
+          span.title = "Reconstruction";
+          span.textContent = "▶️";
+        }
+      }
+      newTD().append(row[FieldName.Comment]);
+    }
+  }
+
+  const button = document.createElement("button");
+  root.prepend(button);
+  button.textContent = "Copy";
+  button.addEventListener("click", () => {
+    navigator.clipboard.writeText(
+      new XMLSerializer().serializeToString(tableRoot)
+    );
+    button.textContent = "Copied!";
+  });
+}
+main();
